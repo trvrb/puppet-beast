0d859153b886e6020d092da1c4f5bfe37b6c1a5a
Trevor Bedford <t.bedford@ed.ac.uk>
2013-03-01 17:04:37 +0000

    Include `java` as a dependency.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,12 @@
 #
 #   include beast
 class beast {
+
+  include java
+
   package { 'BEAST':
     provider => 'appdmg',
     source   => 'https://beast-mcmc.googlecode.com/files/BEAST%20v1.7.5.dmg',
   }
+  
 }
